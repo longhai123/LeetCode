828a53697e4844aa4426235b92dbff97b7796273
Hai Long <longhaiqwe@126.com>
2017-10-10 23:26:29 +0800

    排序 -- 归并排序

--- sorting/sorting.c
+++ sorting/sorting.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <malloc.h>
 
 
 void bubble_sort(int a[], int n)
@@ -61,13 +62,39 @@ void insertionSort(int a[], int N) {
 
 }
 
+void merge(int a[], int low, int mid, int high) {
+    // subarray1 = a[low..mid], subarray2 = a[mid+1..high], both sorted
+    int N = high-low+1;
+    int* b = (int *)malloc(N * sizeof(int)); // discuss: why do we need a temporary array b?
+    int left = low, right = mid+1, bIdx = 0;
+    /* for (int i = low; i < high; i++) */
+        /* printf("%d ", a[i]); */
+    /* printf("\n"); */
+    while (left <= mid && right <= high) // the merging
+        b[bIdx++] = (a[left] <= a[right]) ? a[left++] : a[right++];
+    while (left <= mid) b[bIdx++] = a[left++]; // leftover, if any
+    while (right <= high) b[bIdx++] = a[right++]; // leftover, if any
+    for (int k = 0; k < N; k++) a[low+k] = b[k]; // copy back
+    free(b); // remove the temporary array b
+}
+
+void mergeSort(int a[], int low, int high) {
+    // the array to be sorted is a[low..high]
+    if (low < high) { // base case: low >= high (0 or 1 item)
+        int mid = (low+high) / 2;	
+        mergeSort(a, low  , mid ); // divide into two halves
+        mergeSort(a, mid+1, high); // then recursively sort them
+        merge(a, low, mid, high); // conquer: the merge routine
+    }
+}
 
 int  main()
 {
     int num[8] = {89, 38, 11, 78, 96, 44, 19, 25};
     /* bubble_sort(num, 8); */
     /* selection_sort(num, 8); */
-    insertionSort(num, 8);
+    /* insertionSort(num, 8); */
+    mergeSort(num, 0, 7);
     for(int i=0; i<8; i++)
         printf("%d  ", num[i]);
     printf("\n");
